738bf0b736f152f61465c565c137454c2febd05e
katayama <idnlnroocwetn22@gmail.com>
2025-04-16 22:55:53 +0900

    add a simple response from server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +13,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
