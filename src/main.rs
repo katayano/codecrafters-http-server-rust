ee2aa83c1ce8b296d9007f0c9119316fb26c4113
katayama <idnlnroocwetn22@gmail.com>
2025-04-27 23:22:15 +0900

    modify response according to URI

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,33 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("accepted new connection");
+
+    // Read the Request
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // 200 OK Pattern
+    let get = b"GET / HTTP/1.1\r\n";
+
+    // Write the Response
+    if buffer.starts_with(get) {
+        stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+    } else {
+        stream
+            .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+            .unwrap();
     }
 }
