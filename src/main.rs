ea6ebac87064f7d7a7db7a02212dd7f00ce55f02
katayama <idnlnroocwetn22@gmail.com>
2025-04-29 14:28:37 +0900

    [mod] response user-agent when uri is /user-agent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,21 +8,44 @@ struct Reqeuest {
     method: String,
     uri: String,
     version: String,
+    headers: Vec<HashMap<String, String>>,
+    body: String,
 }
 
 impl Reqeuest {
     fn new(request: &[u8]) -> Self {
         let request_str = String::from_utf8_lossy(request);
-        let request_info = request_str.split_whitespace().collect::<Vec<&str>>();
+        // Split the request into body and (headers + request line)
+        let mut request_info = request_str.split("\r\n\r\n");
 
-        let method = request_info[0].to_string();
-        let uri = request_info[1].to_string();
-        let version = request_info[2].to_string();
+        let req_line_and_headers = request_info.next().unwrap();
+        let mut lines = req_line_and_headers.lines();
+
+        // Request line
+        let line = lines.next().unwrap();
+        let mut request_line = line.split_whitespace();
+        let method = request_line.next().unwrap().to_string();
+        let uri = request_line.next().unwrap().to_string();
+        let version = request_line.next().unwrap().to_string();
+
+        // Headers
+        let mut headers = Vec::new();
+        while let Some(line) = lines.next() {
+            let mut header = HashMap::new();
+            let (key, value) = line.split_once(": ").unwrap();
+            header.insert(key.to_string(), value.to_string());
+            headers.push(header);
+        }
+
+        // Body
+        let body = request_info.next().unwrap_or("").to_string();
 
         Reqeuest {
             method,
             uri,
             version,
+            headers,
+            body,
         }
     }
 }
@@ -69,6 +93,20 @@ fn handle_connection(mut stream: TcpStream) {
                 "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
                 sub_path.len(),
                 sub_path
+            );
+            stream.write(response.as_bytes()).unwrap();
+        }
+        _ if path.starts_with("/user-agent") => {
+            let user_agent = request
+                .headers
+                .iter()
+                .find(|header| header.contains_key("User-Agent"))
+                .and_then(|header| header.get("User-Agent").cloned())
+                .unwrap_or("".to_string());
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                user_agent.len(),
+                user_agent
             );
             stream.write(response.as_bytes()).unwrap();
         }
